89e0f0c7c1c6ed9fbb817c9f28f804d8321c1f55
stamats <Matthias.Kohl@stamats.de>
2019-11-08 10:33:38 +0100

    simCorVars gained arguments for mean and SD

--- R/simCorVars.R
+++ R/simCorVars.R
@@ -1,4 +1,4 @@
-simCorVars <- function(n, r, plot = TRUE){
+simCorVars <- function(n, r, mu1=0, mu2=0, sd1=1, sd2=1, plot = TRUE){
   if(!is.integer(n)) n <- as.integer(n)
   if(length(n) > 1){
     n <- n[1]
@@ -10,15 +10,19 @@ simCorVars <- function(n, r, plot = TRUE){
     r <- r[1]
     warning("length(r) > 1: only the first entry is used.")
   }
+  stopifnot(is.numeric(mu1), length(mu1) == 1, 
+            is.numeric(mu2), length(mu2) == 1,
+            is.numeric(sd1), length(sd1) == 1, 
+            is.numeric(sd2), length(sd1) == 1)
   x1 <- rnorm(n)
   y1 <- rnorm(n)
   a <- sqrt((1-r)/(1+r))
   if(abs(r) == 1){
-    x <- x1
-    y <- sign(r)*x1
+    x <- x1*sd1 + mu1
+    y <- sign(r)*x1*sd2 + mu2
   }else{
-    x <- x1 + a*y1
-    y <- x1 - a*y1
+    x <- (x1 + a*y1)*sd1 + mu1
+    y <- (x1 - a*y1)*sd2 + mu2
   }
   if(plot) plot(x, y, main = paste("Correlation =", r),
                 xlab = "Variable 1", ylab = "Variable 2")
