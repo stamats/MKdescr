88bff54ea17d6875d1db91add3a5a91c6ad12409
stamats <Matthias.Kohl@stamats.de>
2020-10-18 19:22:34 +0200

    added new arguments

--- R/CV.R
+++ R/CV.R
@@ -6,18 +6,18 @@ CV <- function(x, na.rm = FALSE){
   sd(x)/mean(x)
 }
 
-medCV <- function(x, na.rm = FALSE){
+medCV <- function(x, na.rm = FALSE, constant = 1/qnorm(0.75)){
   stopifnot(is.numeric(x))
   if(na.rm) x <- x[!is.na(x)]
   if(any(x <= 0))
     stop("Your data must be positive!")
-  mad(x)/median(x)
+  mad(x, constant = constant)/median(x)
 }
 
-iqrCV <- function(x, na.rm = FALSE){
+iqrCV <- function(x, na.rm = FALSE, type = 7, constant = 2*qnorm(0.75)){
   stopifnot(is.numeric(x))
   if(na.rm) x <- x[!is.na(x)]
   if(any(x <= 0))
     stop("Your data must be positive!")
-  sIQR(x)/median(x)
+  sIQR(x, type = type, constant = constant)/median(x)
 }
--- R/SNR.R
+++ R/SNR.R
@@ -5,16 +5,16 @@ SNR <- function(x, na.rm = FALSE){
   mean(x, na.rm = na.rm)/sd(x, na.rm = na.rm)
 }
 
-medSNR <- function(x, na.rm = FALSE){
+medSNR <- function(x, na.rm = FALSE, constant = 1/qnorm(0.75)){
   stopifnot(is.numeric(x))
   if(any(x <= 0))
     stop("Your data must be positive!")
-  median(x, na.rm = na.rm)/mad(x, na.rm = na.rm)
+  median(x, na.rm = na.rm)/mad(x, na.rm = na.rm, constant = constant)
 }
 
-iqrSNR <- function(x, na.rm = FALSE){
+iqrSNR <- function(x, na.rm = FALSE, type = 7, constant = 2*qnorm(0.75)){
   stopifnot(is.numeric(x))
   if(any(x <= 0))
     stop("Your data must be positive!")
-  median(x, na.rm = na.rm)/sIQR(x, na.rm = na.rm)
+  median(x, na.rm = na.rm)/sIQR(x, na.rm = na.rm, type = type, constant = constant)
 }
