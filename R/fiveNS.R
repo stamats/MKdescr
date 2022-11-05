3ecacbac323e8ce7b6823087e3b2bfc624978aa3
stamats <matthias.kohl@stamats.de>
2022-11-05 11:04:48 +0100

    package checks without notes, warnings, errors

--- R/fiveNS.R
+++ R/fiveNS.R
@@ -11,9 +11,9 @@ fiveNS <- function(x, na.rm = TRUE, type = 7){
     return(rep.int(NA, 5))
   else{
     return(c('Minimum' = min(x, na.rm = na.rm), 
-             '1.Quartile' = quantile(x, prob = 0.25, type = type, na.rm = na.rm),
+             quantile(x, prob = 0.25, type = type, na.rm = na.rm),
              'Median' = median(x, na.rm = na.rm),
-             '3.Quartile' = quantile(x, prob = 0.75, type = type, na.rm = na.rm),
+             quantile(x, prob = 0.75, type = type, na.rm = na.rm),
              'Maximum' = max(x, na.rm = na.rm)))
   }
 }
