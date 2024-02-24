5ea48a8ae29d63d3d29fb56ef1a78b562717f615
stamats <matthias.kohl@stamats.de>
2024-02-24 11:23:22 +0100

    added breaks functions for hopefully better labeling of breaks for glog

--- R/transformations.R
+++ R/transformations.R
@@ -1,8 +1,74 @@
-library(scales)
+## adapted from scales:::log_breaks
+glog_breaks <- function (n = 5, base = 10) {
+  list(n, base)
+  n_default <- n
+  function(x, n = n_default) {
+    raw_rng <- suppressWarnings(range(x, na.rm = TRUE))
+    if (any(!is.finite(raw_rng))) {
+      return(numeric())
+    }
+    rng <- glog(raw_rng, base = base)
+    min <- floor(rng[1])
+    max <- ceiling(rng[2])
+    if (max == min) {
+      return(inv.glog(min, base = base))
+    }
+    by <- floor((max - min)/n) + 1
+    breaks <- inv.glog(seq(min, max, by = by), base = base)
+    relevant_breaks <- inv.glog(rng[1], base = base) <= breaks & breaks <= inv.glog(rng[2], base = base)
+    if (sum(relevant_breaks) >= (n - 2)) {
+      return(breaks)
+    }
+    while (by > 1) {
+      by <- by - 1
+      breaks <- inv.glog(seq(min, max, by = by), base = base)
+      relevant_breaks <- inv.glog(rng[1], base = base) <= breaks & breaks <= inv.glog(rng[2], base = base)
+      if (sum(relevant_breaks) >= (n - 2)) {
+        return(breaks)
+      }
+    }
+    glog_sub_breaks(rng, n = n, base = base)
+  }
+}
+## adapted from scales:::glog_sub_breaks
+glog_sub_breaks <- function (rng, n = 5, base = 10){
+  min <- floor(rng[1])
+  max <- ceiling(rng[2])
+  if (base <= 2) {
+    return(inv.glog(min:max, base = base))
+  }
+  steps <- 1
+  delta <- function(x) {
+    min(diff(glog(sort(c(x, steps, base)), base = base)))
+  }
+  candidate <- seq_len(base)
+  candidate <- candidate[1 < candidate & candidate < base]
+  while (length(candidate)) {
+    best <- which.max(vapply(candidate, delta, 0))
+    steps <- c(steps, candidate[best])
+    candidate <- candidate[-best]
+    breaks <- as.vector(outer(inv.glog(seq(min, max), base = base), steps))
+    relevant_breaks <- inv.glog(rng[1], base = base) <= breaks & breaks <= inv.glog(rng[2], base = base)
+    if (sum(relevant_breaks) >= (n - 2)) {
+      break
+    }
+  }
+  if (sum(relevant_breaks) >= (n - 2)) {
+    breaks <- sort(breaks)
+    lower_end <- pmax(min(which(inv.glog(rng[1], base = base) <= breaks)) - 1, 1)
+    upper_end <- pmin(max(which(breaks <= inv.glog(rng[2], base = base))) + 1, length(breaks))
+    breaks[lower_end:upper_end]
+  }
+  else {
+    extended_breaks(n = n)(inv.glog(rng, base = base))
+  }
+}
 glog_trans <- function(base = exp(1)){
   trans_new("glog",
             transform = function(x) glog(x, base = base),
-            inverse = function(x) inv.glog(x, base = base))
+            inverse = function(x) inv.glog(x, base = base),
+            breaks = glog_breaks(base = base),
+            domain = c(-Inf, Inf))
 }
 glog10_trans <- function(){ glog_trans(base = 10) }
 glog2_trans <- function(){ glog_trans(base = 2) }
